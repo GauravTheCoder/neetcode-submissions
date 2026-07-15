3da7c8b7cb9d9ee8804fd26657d58c94e9cd9bd9
GauravS <50472848+GauravTheCoder@users.noreply.github.com>
2026-07-15 16:16:26 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+        for(int i{0};i<nums.size();++i) {
+            int partner = target - nums[i];
+            if(seen.find(partner)!=seen.end()) {
+                return{seen[partner], i};
+            }
+        seen[nums[i]] = i;
+        }
+        return{};
+    }
+};
